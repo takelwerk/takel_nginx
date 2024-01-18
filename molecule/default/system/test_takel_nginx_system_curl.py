c43ec207bc6f0c327f202b50375fc3b2de73995d
Stephan <stephan@takelwerk.net>
2024-01-18 21:26:20 +0100

    Update hg repos

---
+++ molecule/default/system/test_takel_nginx_system_curl.py
@@ -0,0 +1,38 @@
+import takeltest
+
+# system test server
+testinfra_hosts = [f"{takeltest.hosts()[0]}-system"]
+
+
+def test_takel_nginx_system_web_http(host, testvars):
+    ip = testvars['ansible_all_ipv4_addresses'][0]
+    curl_command = ("curl "
+                    "--head "
+                    "--insecure "
+                    "--location "
+                    "--output /dev/null "
+                    "--silent "
+                    "--write-out '%{http_code}\n'")
+    http_code = host.check_output(f"{curl_command} {ip}")
+    valid_return_codes = [
+        '200',
+        '301'
+    ]
+    assert http_code in valid_return_codes
+
+
+def test_takel_nginx_system_nginx_https(host, testvars):
+    ip = testvars['ansible_all_ipv4_addresses'][0]
+    curl_command = ("curl "
+                    "--head "
+                    "--insecure "
+                    "--location "
+                    "--output /dev/null "
+                    "--silent "
+                    "--write-out '%{http_code}\n'")
+    http_code = host.check_output(f"{curl_command} {ip}")
+    valid_return_codes = [
+        '200',
+        '301'
+    ]
+    assert http_code in valid_return_codes
